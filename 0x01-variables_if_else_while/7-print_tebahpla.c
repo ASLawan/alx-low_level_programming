0c71fb3af717ccf5a9a601609deecbf753efdff1
ASLawan <nawalnitsua@gmail.com>
2023-08-11 08:07:07 -0700

    Print in alphabet in lowercasem reverse order

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int j;
+
+	for (j = 20; j > 96; j--)
+	{
+		putchar(j);
+	}
+	putchar('\n');
+	return (0);
+}
