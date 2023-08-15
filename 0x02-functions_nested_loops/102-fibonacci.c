268e582f09762c1e9ed490987f1d1393afa20e05
ASLawan <nawalnitsua@gmail.com>
2023-08-15 14:40:27 -0700

    Print Fibonacci series

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - prints firts 50 fibonacci series numbers
+ *
+ * Return: fibonacci series
+ */
+
+int main(void)
+{
+int num, first, second, next;
+
+first = 1;
+second = 2;
+printf("%d, ", first);
+printf("%d, ", second);
+for (num = 0; num < 48; num++)
+{
+next = first + second;
+printf("%d, ", next);
+first = second;
+second = next;
+}
+printf("\n");
+return (0);
+}
