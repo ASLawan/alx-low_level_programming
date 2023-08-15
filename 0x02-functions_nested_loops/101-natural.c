c87135c765d43e0dc38024535e55a906e59f6930
ASLawan <nawalnitsua@gmail.com>
2023-08-15 14:04:05 -0700

    Add multiples of 3 and 5 below 1024

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Prints natural numbers below 1024 that are
+ * multiplies of 3 or 5
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+int num = 1024, n = 0, sum = 0;
+
+while (n < num)
+{
+if (n % 5 == 0 || n % 3 == 0)
+{
+sum += n;
+}
+n++;
+}
+printf("%d", sum);
+return (0);
+}
