c3105577c694039a42344aba8ac470eeb1bc91fc
ASLawan <nawalnitsua@gmail.com>
2023-08-17 09:55:17 -0700

    Print newline if size <= 0

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -12,16 +12,23 @@ void print_triangle(int size)
 {
 	int i, j, k;
 
-	for (i = 0; i < size; i++)
+	if (size <= 0)
 	{
-		for (j = 0; j < (size - i - 1); j++)
-		{
-			_putchar(32);
-		}
-		for (k = 0; k <= i; k++)
-		{
-			_putchar(35);
-		}
 		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < (size - i - 1); j++)
+			{
+				_putchar(32);
+			}
+			for (k = 0; k <= i; k++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
 	}
 }
