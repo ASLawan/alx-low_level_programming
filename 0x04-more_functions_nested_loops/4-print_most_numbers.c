eb3f179ca9041ac5622a659d494d619dc7802506
ASLawan <nawalnitsua@gmail.com>
2023-08-17 01:22:41 -0700

    Add: function to print single digits except 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints 0 to 9 but not 2 and 4
+ *
+ * Return: numbers
+ */
+
+void print_most_numbers(void)
+{
+	char i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		if (i != 2 || i != 4)
+		{
+			_putchar(i);
+		}
+	}
+	_putchar('\n');
+}
