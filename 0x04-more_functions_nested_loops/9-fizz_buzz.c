110ce6bb84a5fb48340197acc91ea258050b63a4
ASLawan <nawalnitsua@gmail.com>
2023-08-17 09:03:09 -0700

    Ad: program to print FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - prints integers and fizzbuzz
+ *
+ * Return: integers
+ */
+
+int main(void)
+{
+	int i;
+	char str1[] = "FizzBuzz";
+	char str2[] = "Fizz";
+	char str3[] = "Buzz";
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 5 == 0 && i % 3 == 0)
+		{
+			printf("%s ", str1);
+		}
+		else if (i % 5 == 0)
+		{
+			printf("%s ", str3);
+		}
+		else if (i % 3 == 0)
+		{
+			printf("%s ", str2);
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	return (0);
+}
