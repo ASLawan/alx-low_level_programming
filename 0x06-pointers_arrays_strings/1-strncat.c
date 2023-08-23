236d0dd446d241f00b9426b7d986d28e348413a3
ASLawan <nawalnitsua@gmail.com>
2023-08-23 01:25:14 -0700

    Concatenate two strings upto the given number of bytes

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * *_strncat - concatenates two string upto a given bytes
+ *
+ * @src: string to concatenate with
+ * @dest: string to concatenate
+ * @n: bytes to contenate
+ *
+ * Return: concatenated string
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int len1 = 0, i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+		len1 += 1;
+	}
+
+	for (j = 0; j < n; j++)
+	{
+		dest[len1 + j] = src[j];
+	}
+	return (dest);
+}
