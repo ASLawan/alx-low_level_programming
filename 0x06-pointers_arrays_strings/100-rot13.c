2827283fb1959ef47d99941b235cff08a7ac808c
ASLawan <nawalnitsua@gmail.com>
2023-08-23 13:12:10 -0700

    Encode string using rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * *rot13 - shift character 13 places
+ *
+ * @s: input string
+ *
+ * Return: ciphered text
+ */
+
+char *rot13(char *s)
+{
+	int i, rot;
+
+	rot  = 13;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			if ((s[i] + rot) <= 122)
+			{
+				s[i] += rot;
+			}
+			else
+			{
+				s[i] -= rot;
+			}
+		}
+		if (s[i] >= 'A' && s[i] <= 'Z')
+		{
+			if ((s[i] + rot) <= 90)
+			{
+				s[i] += rot;
+			}
+			else
+			{
+				s[i] -= rot;
+			}
+		}
+
+	}
+	return (s);
+}
