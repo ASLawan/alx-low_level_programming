27699db85c910a2bea9883da2d54496be6813c8f
ASLawan <nawalnitsua@gmail.com>
2023-09-01 13:58:14 +0100

    Write a program that prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints name of file
+ *
+ * @argv: array of arguments
+ * @argc: array size
+ *
+ * Return: Always 0;
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", *argv);
+
+	return (0);
+}
