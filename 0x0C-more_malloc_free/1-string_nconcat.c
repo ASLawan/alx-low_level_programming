bb494585fff3b11220f019120552c12ac4494587
ASLawan <nawalnitsua@gmail.com>
2023-09-07 13:50:25 +0100

    Handle NULL arguments

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -19,6 +19,14 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int len1 = strlen(s1);
 	unsigned int len2 = strlen(s2);
 
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
 	if (n >= len2)
 	{
 		n = len2;
@@ -40,5 +48,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		new_str[len1 + i] = s2[i];
 	}
 	new_str[new_str_len] = '\0';
+
 	return (new_str);
 }
