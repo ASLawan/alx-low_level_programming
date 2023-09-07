ab8c419f3be83d7f7ca8a39f42b4f3bfc5407ac9
ASLawan <nawalnitsua@gmail.com>
2023-09-07 14:27:39 +0100

    Concatenate two strings using malloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,23 +1,22 @@
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 /**
- * *string_nconcat - concatenates two strings
+ * *string_nconcat - concatenate s1 and n bytes from s2
  *
  * @s1: string one
  * @s2: string two
- * @n: bytes
+ * @n: number of bytes
  *
- * Return: string
+ * Return: concatenated string
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *new_str;
-	unsigned int i, new_str_len;
-	unsigned int len1 = strlen(s1);
-	unsigned int len2 = strlen(s2);
+	unsigned int i, len1 = 0, len2 = 0;
 
 	if (s1 == NULL)
 	{
@@ -27,27 +26,33 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		s2 = "";
 	}
+
+	len1 = strlen(s1);
+	len2 = strlen(s2);
+
 	if (n >= len2)
 	{
 		n = len2;
 	}
-	new_str_len = len1 + n;
 
-	new_str = malloc((new_str_len + 1) * sizeof(char));
+	new_str = malloc(sizeof(char) * (len1 + n + 1));
 
 	if (new_str == NULL)
 	{
 		return (NULL);
 	}
+
 	for (i = 0; i < len1; i++)
 	{
 		new_str[i] = s1[i];
 	}
+
 	for (i = 0; i < n; i++)
 	{
 		new_str[len1 + i] = s2[i];
 	}
-	new_str[new_str_len] = '\0';
+
+	new_str[len1 + n] = '\0';
 
 	return (new_str);
 }
