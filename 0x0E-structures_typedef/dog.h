dd193137853042070b338b32edd57db561e8e01e
ASLawan <nawalnitsua@gmail.com>
2023-09-11 16:57:48 +0100

    Define a new type of struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+/**
+ * struct dog - dog strcuture
+ * @name: name member
+ * @age: age member
+ * @owner: owner member
+ *
+ * Description: dog information
+ */
+
+struct dog 
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+#endif
