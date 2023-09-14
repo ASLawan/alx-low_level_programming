100adbb0ab90d7ded29cff0e2b6d8a7cd16ccb96
ASLawan <nawalnitsua@gmail.com>
2023-09-14 14:20:50 +0100

    Ensure correct function output

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -13,43 +13,42 @@
 
 void print_all(const char * const format, ...)
 {
-	int i = 0, num;
-	char *str, letter;
-	double decimal;
-	va_list arguments;
+	va_list args;
+	const char *p = format;
+	char c;
+	int i;
+	double f;
+	char *s;
 
-	va_start(arguments, format);
-	while (format[i])
+	va_start(args, format);
+	while (*p)
 	{
-		switch (format[i++])
+		switch (*p++)
 		{
 			case 'c':
-				letter = (char) va_arg(arguments, int);
-				printf("%c", letter);
+				c = (char) va_arg(args, int);
+				printf("%c", c);
 				break;
 			case 'i':
-				num = va_arg(arguments, int);
-				printf("%d", num);
+				i = va_arg(args, int);
+				printf("%d", i);
+				break;
+			case 'f':
+				f = va_arg(args, double);
+				printf("%f", f);
 				break;
 			case 's':
-				str = va_arg(arguments, char *);
-				if (str == NULL)
+				s = va_arg(args, char*);
+				if (s == NULL)
 				{
 					printf("(nil)");
 				}
-				printf("%s", str);
-				break;
-			case 'f':
-				decimal = va_arg(arguments, double);
-				printf("%f", decimal);
+				printf("%s", s);
 				break;
 		}
-		if ((format[i] == 'c' || format[i] == 'i' || format[i] == 'f'\
-				 || format[i] == 's') && (format[i] != '\0'))
-		{
+		if ((*p == 'c' || *p == 'i' || *p == 'f' || *p == 's') && (*p != '\0'))
 			printf(", ");
-		}
 	}
-	va_end(arguments);
 	printf("\n");
+	va_end(args);
 }
