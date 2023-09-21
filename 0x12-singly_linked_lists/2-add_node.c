3a98d244bf339eb42f5c2e26e7e0e977a252ec2e
ASLawan <nawalnitsua@gmail.com>
2023-09-21 18:27:32 +0100

    Make file betty compliant

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -12,16 +12,16 @@ list_t *add_node(list_t **head, const char *str)
 {
 	/*create new node*/
 	list_t *new_node;
-	
+
 	/*allocate memory to new node*/
 	new_node = malloc(sizeof(list_t));
-	
+
 	/*verify the allocated memory*/
 	if (new_node == NULL)
 	{
 		return (NULL);
 	}
-	
+
 	/*duplicate the given string*/
 	new_node->str = strdup(str);
 	if (new_node->str == NULL)
@@ -35,7 +35,7 @@ list_t *add_node(list_t **head, const char *str)
 
 	/*set new node pointer to point to first node*/
 	new_node->next = *head;
-	
+
 	/*set head to point to new node*/
 	*head = new_node;
 
