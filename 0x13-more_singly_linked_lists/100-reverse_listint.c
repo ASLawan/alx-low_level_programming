ea0a80a1b28247fa2339789dc15ac337d5863586
ASLawan <nawalnitsua@gmail.com>
2023-09-26 05:58:42 +0100

    Work within requirements. Make file betty compliant

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -10,15 +10,15 @@
 listint_t *reverse_listint(listint_t **head)
 {
 	listint_t *prev_node = NULL;
-	listint_t *current_node = *head;
+	listint_t *current_node = NULL;
 
-	while (current_node != NULL)
+	while (*head != NULL)
 	{
-		listint_t *next_node = current_node->next;
+		current_node = (*head)->next;
+		(*head)->next = prev_node;
+		prev_node = *head;
 
-		current_node->next = prev_node;
-		prev_node = current_node;
-		current_node = next_node;
+		*head = current_node;
 	}
 
 	*head = prev_node;
