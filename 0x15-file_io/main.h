59003ac7c5dc05aaa8446f59d2e1560d1909b22e
ASLawan <nawalnitsua@gmail.com>
2023-10-02 13:46:52 +0100

    Add library header files to header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/************ HEADER FILES *************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+/************ FUNCTION PROTOTYPES **************/
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
