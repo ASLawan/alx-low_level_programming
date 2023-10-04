b870a2bdc879382e537ce8e938b572c0d633cfed
ASLawan <nawalnitsua@gmail.com>
2023-10-04 06:22:48 +0100

    Redo task 5

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,95 +1,64 @@
 #include "main.h"
-
 /**
- * print_hdr - prints elf header
- * @hdr: elf file
+ * print_elf_header - prints elf header info
+ * @hdr: elf header structure
  * Return: nothing
  */
-void print_hdr(struct elf_hdr *hdr)
+void print_elf_header(struct elf_hdr *hdr)
 {
-printf("ELF header information:\n");
-printf("Magic: %02x %02x %02x %02x\n", hdr->e_ident[EI_MAG0], hdr->e_ident[EI_MAG1], hdr->e_ident[EI_MAG2], hdr->e_ident[EI_MAG3]);
-printf("Class: ");
-switch (hdr->e_ident[EI_CLASS])
-{
-case ELFCLASS32:
-printf("ELF32\n");
-break;
-case ELFCLASS64:
-printf("ELF64\n");
-break;
-default:
-printf("Unknown\n");
-break;
-}
-printf("Data: ");
-switch (hdr->e_ident[EI_DATA])
-{
-case ELFDATA2LSB:
-printf("Little-endian\n");
-break;
-case ELFDATA2MSB:
-printf("Big-endian\n");
-break;
-default:
-printf("Unknown\n");
-break;
-}
-printf("Version: %u\n", hdr->e_version);
-printf("OS/ABI: %u\n", hdr->e_ident[EI_OSABI]);
-printf("ABI Version: %u\n", hdr->e_ident[EI_ABIVERSION]);
-printf("Type: ");
-switch (hdr->e_type)
-{
-case ET_EXEC:
-printf("Executable file\n");
-break;
-case ET_DYN:
-printf("Shared object file\n");
-break;
-case ET_REL:
-printf("Relocatable file\n");
-break;
-default:
-printf("Unknown\n");
-break;
-}
-printf("Entry point address: 0x%016lx\n", hdr->e_entry);
+	int i;
+
+printf("ELF Header:\n");
+printf("  Magic:   ");
+for (i = 0; i < EI_NIDENT; i++)
+printf("%02x ", hdr->e_ident[i]);
+printf("\n");
+printf("  Class:                             ELF64\n");
+printf("  Data:                              2's complement, little endian\n");
+printf("  Version:                           1 (current)\n");
+printf("  OS/ABI:                            UNIX - System V\n");
+printf("  ABI Version:                       0\n");
+printf("  Type:                              EXEC (Executable file)\n");
+printf("  Entry point address:               0x%lx\n", hdr->e_entry);
 }
 /**
- * main - entry ppoint
+ * main - program entry point
  * @argc: number of arguments
- * @argv: array of arguments
- * Return: 0, always success
+ * @argv: argument array
+ * Return: 0 always success
  */
 int main(int argc, char **argv)
 {
-FILE *file;
-struct elf_hdr hdr;
+	int fd;
+	struct elf_hdr elf_header;
 if (argc != 2)
 {
 fprintf(stderr, "Usage: elf_header elf_filename\n");
-return (1);
+return (98);
 }
-
-file = fopen(argv[1], "rb");
-if (file == NULL)
+fd = open(argv[1], O_RDONLY);
+if (fd == -1)
 {
-fprintf(stderr, "Error opening file: %s\n", argv[1]);
-return (1);
+perror("Error opening file");
+return (98);
 }
-
-fseek(file, 0, SEEK_SET);
-fread(&hdr, sizeof(hdr), 1, file);
-fclose(file);
-
-if (memcmp(hdr.e_ident, ELFMAG, SELFMAG) != 0)
+if (read(fd, &elf_header, sizeof(elf_header)) != sizeof(elf_header))
 {
-fprintf(stderr, "Error: File is not an ELF file.\n");
+fprintf(stderr, "Error reading ELF header\n");
 return (98);
 }
 
-print_hdr(&hdr);
+if (elf_header.e_ident[EI_MAG0] != ELFMAG0 ||
+elf_header.e_ident[EI_MAG1] != ELFMAG1 ||
+elf_header.e_ident[EI_MAG2] != ELFMAG2 ||
+elf_header.e_ident[EI_MAG3] != ELFMAG3)
+{
+fprintf(stderr, "File is not an ELF file\n");
+return (98);
+}
 
+print_elf_header(&elf_header);
+
+close(fd);
 return (0);
 }
