378398c28715bc20c8e68fd12caed8f00d548124
ASLawan <nawalnitsua@gmail.com>
2023-11-09 23:09:39 +0100

    Fix betty requirement error

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -7,7 +7,16 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t count = 0;
-	
+
+	if (h == NULL)
+	{
+		return (count);
+	}
+	while (h->prev != NULL)
+	{
+		h = h->prev;
+	}
+
 	while (h != NULL)
 	{
 		printf("%u\n", h->n);
